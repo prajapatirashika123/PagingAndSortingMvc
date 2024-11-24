474d50757b5cbce7cc26bcba81ba90bd0c54dbd7
Shanu <shanu12pr@gmail.com>
2024-11-24 14:11:16 +0530

    Add project files.

---
+++ PagingAndSorting/Entities/EmployeeMaster.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PagingAndSorting.Entities
+{
+    public class EmployeeMaster
+    {
+        [Required(ErrorMessage = "Please Enter Email")]
+        public string Email { get; set; }
+
+        [Key]
+        public string ID { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Employee Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Salary")]
+        public decimal Salary { get; set; }
+    }
+}
---
+++ PagingAndSorting/Models/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PagingAndSorting.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PagingAndSorting.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<EmployeeMaster> Employees { get; set; }
+    }
+}
